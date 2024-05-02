43ed4491f0d9c1e3124a8d16774e78a63b1b6a13
Jhonny Silva <matias.silva.200517@gmail.com>
2024-05-02 12:16:52 -0400

    commit

---
+++ assets/js/script2.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+    function obtenerDigimones(){
+        $('#obtenerDigimones').click(function(){
+            $.ajax({
+                url:'https://digimon-api.vercel.app/api/digimon',
+                type:'GET',
+                success:function(data){
+                    mostrarDigimones(data);
+                },
+                error:function(xhr,status,error){
+                    console.error('Error al obtener los datos:',error);
+                }
+            });
+        });
+    }
+    //FUNCION PARA MOSTRAR LOS DIGIMON EN CARDS
+function mostrarDigimones(digimones){
+    let digimonList = '';
+    $.map(digimones,function(digimon){
+        let card = `
+        <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card">
+                <img src="${digimon.img}" alt="${digimon.name}" class="card-img-top">
+                <div class="card-body"> 
+                    <h5 class="card-title">${digimon.name}</h5>
+                    <p class="card-text">Nivel: ${digimon.level}</p>
+                    <p class="card-text">Tipo: ${digimon.type}</p>
+                </div>
+            </div>
+        </div>`;
+        digimonList=digimonList +card;
+    });
+    $('#digimonList').html(digimonList);
+}
+//Ejecturar la funcion para mostrar los digimon
+obtenerDigimones();
+});
+
---
+++ assets/js/script4.js
@@ -0,0 +1,8 @@
+const obtenerDigimonAsyncsAwait=async()=>{
+    try{
+        const responde =await fetch ('https://digimon-api.vercel.app/api/digimon');
+        if(!response.ok){
+            throw
+        }
+    }
+}
